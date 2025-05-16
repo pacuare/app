c2ca7cbb34f903c731e67edbc94f38a3794207fd
Aleks Rutins <keeper@farthergate.com>
2025-05-16 11:23:31 -0400

    Add healthcheck

--- api/router.go
+++ api/router.go
@@ -1,10 +1,19 @@
 package api
 
 import (
+	"fmt"
+	"net/http"
+
 	"app.pacuare.dev/api/auth"
 	"github.com/jackc/pgx/v5"
 )
 
 func Mount(conn *pgx.Conn) {
 	auth.Mount(conn)
+
+	http.HandleFunc("GET /api/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(200)
+		fmt.Fprint(w, `{"ok":"true"}`)
+	})
 }
